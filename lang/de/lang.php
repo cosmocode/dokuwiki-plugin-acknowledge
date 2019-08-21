c1d6d38c7dcb712fccde4eccadb5e4d78c2a841f
Andreas Gohr <gohr@cosmocode.de>
2019-08-21 15:33:01 +0200

    added German translation

---
+++ lang/de/lang.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * German language file for acknowledge plugin
+ *
+ * @author Andreas Gohr, Anna Dabrowska <dokuwiki@cosmocode.de>
+ */
+
+// menu entry for admin plugins
+ $lang['menu'] = 'Bestätigungsüberblick';
+
+// custom language strings for the plugin
+$lang['ackRequired'] = 'Sie müssen diese Seite noch bestätigen';
+$lang['ackText'] = 'Ich bestätige den Inhalt dieser Seite gelesen und verstanden zu haben';
+$lang['ackButton'] = 'Bestätigen';
+$lang['ackGranted'] = 'Sie haben diese Seite bestätigt am ';
+$lang['ackNotFound'] = 'Nichts gefunden';
+$lang['overviewPage'] = 'Seite';
+$lang['overviewUser'] = 'Nutzer';
+$lang['overviewTime'] = 'Letzte Bestätigung';
+$lang['overviewHistory'] = 'Sie können ältere Bestätigungen über die Datenbank abfragen. Bitten Sie Ihren Administrator um Hilfe.';
+
+
+$lang['error sqlite plugin missing'] = 'Das sqlite plugin ist nicht installiert';
