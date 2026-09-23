0757911cd785513b2e169a56471278f0575156be
Szymon Olewniczak <szyolewn@pg.edu.pl>
2026-09-23 12:58:07 +0200

    polish translation

---
+++ lang/pl/lang.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * Polish language file for acknowledge plugin
+ *
+ * @author Andreas Gohr, Anna Dabrowska <dokuwiki@cosmocode.de>
+ */
+
+// menu entry for admin plugins
+$lang['menu'] = 'Ostatnie zapoznania';
+$lang['menu_assign'] = 'Przypisywanie obowiązku zapoznania';
+$lang['menu_stats'] = 'Statystyki zapoznania się z treścią';
+
+// custom language strings for the plugin
+$lang['ackRequired'] = 'Wymagane jest zapoznanie się z tą stroną';
+$lang['ackText'] = 'Oświadczam, że przeczytałem(am) i zrozumiałem(am) treść tej strony';
+$lang['ackButton'] = 'Zapoznałem/am się';
+$lang['ackOk'] = 'Potwierdzono zapoznanie się ze stroną';
+$lang['ackGranted'] = 'Potwierdzono zapoznanie się ze stroną dnia %s';
+$lang['ackDiff'] = 'Pokaż zmiany od ostatniego zapoznania się z dnia %s';
+$lang['ackNotFound'] = 'Nic nie znaleziono';
+$lang['reportTitle'] = 'Raport zapoznania się z treścią';
+$lang['reportAcknowledgedTitle'] = 'Użytkownicy, którzy zapoznali się ze stroną';
+$lang['reportPendingTitle'] = 'Użytkownicy, którzy nadal muszą zapoznać się ze stroną';
+$lang['reportNobody'] = '(brak)';
+$lang['reportUserCount'] = '%d użytkowników (pokaż)';
+$lang['reportAckedOn'] = 'dnia';
+$lang['overviewPage'] = 'Strona';
+$lang['overviewUser'] = 'Użytkownik';
+$lang['overviewTime'] = 'Ostatnie zapoznanie się';
+$lang['overviewMod'] = 'Ostatnia zmiana';
+$lang['overviewCurrent'] = 'Aktualne?';
+$lang['overviewHistory'] = 'Dostęp do historii zapoznań można uzyskać poprzez bezpośrednie zapytanie do bazy danych. Poproś administratora o pomoc.';
+$lang['count'] = 'Użytkownik %s zapoznał się z %d z %d stron.';
+$lang['yes'] = 'tak';
+$lang['home'] = 'Najnowsze zapoznania';
+$lang['status'] = 'Status';
+$lang['all'] = 'wszystkie';
+$lang['current'] = 'aktualne';
+$lang['due'] = 'wymagane zapoznanie';
+
+$lang['toomanyresults'] = 'Zbyt wiele wyników do wyświetlenia. Wyniki zostały ograniczone.';
+
+$lang['statsTotal'] = 'Całe wiki';
+$lang['statsTotalLink'] = 'Powrót do głównej przestrzeni nazw';
+$lang['statsNamespace'] = 'Przestrzeń nazw';
+$lang['statsSubnamespace'] = 'Podprzestrzenie nazw';
+$lang['statsRoot'] = '(główny poziom)';
+$lang['statsHere'] = '(strony w tej przestrzeni nazw)';
+$lang['statsAcked'] = 'Zapoznano';
+$lang['statsRequired'] = 'Wymagane zapoznanie';
+$lang['statsRatio'] = 'Stopień realizacji';
+$lang['statsPages'] = 'Przypisane strony';
+
+$lang['pattern'] = 'Wzorzec strony';
+$lang['assignees'] = 'Przypisane osoby/grupy';
+$lang['save'] = 'Zapisz';
+
+$lang['error sqlite plugin missing'] = 'Brak wtyczki sqlite';
+
+$lang['notification'] = 'Wymagane jest potwierdzenie zapoznania się z tą stroną: %s';
---
+++ lang/pl/settings.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Polish language file for acknowledge plugin (settings)
+ *
+ * @author Andreas Gohr, Anna Dabrowska <dokuwiki@cosmocode.de>
+ */
+
+$lang['approve_integration'] = 'Jeśli wtyczka approve jest zainstalowana, ukryj przycisk ' .
+    'zapoznania się ze stronami podlegającymi zatwierdzeniu, które nie są w stanie „Zatwierdzona”.';
+$lang['notification_integration'] = 'Jeśli wtyczka notification jest zainstalowana, wysyłaj e-maile ' .
+    'do przypisanych użytkowników o stronach wymagających ich zapoznania się z treścią.';
+$lang['onpage_report'] = 'Pokaż na stronie raport informujący o tym, kto zapoznał się z jej treścią ' .
+    'i/lub kto nadal musi to zrobić. Raport jest widoczny tylko dla menedżerów i administratorów.';
+$lang['onpage_report_o_off'] = 'Wyłączony';
+$lang['onpage_report_o_acknowledged'] = 'Użytkownicy, którzy zapoznali się ze stroną';
+$lang['onpage_report_o_pending'] = 'Użytkownicy, którzy muszą zapoznać się ze stroną';
+$lang['onpage_report_o_both'] = 'Zarówno użytkownicy, którzy zapoznali się, jak i oczekujący';
